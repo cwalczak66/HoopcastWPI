8fc9ff760bb276f0ab73b96f5f1bb091ea92b9e5
harbar20 <harbar2021@gmail.com>
2024-10-02 19:49:27 -0400

    flask server has begun

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+
+@app.route("/favicon.ico")
+def favicon():
+    return ""
+
+@app.route('/')
+def index():
+    return render_template('index.html')
